d0b08f0597558cd21b71594bcd8a0019008aed6b
prakash <prakash@mymail.com>
2024-05-07 17:10:14 +0530

    modifed ec2 added output

--- ec2.tf
+++ ec2.tf
@@ -3,8 +3,19 @@ resource "aws_instance" "myec2" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.mysg.id]
   subnet_id              = aws_subnet.mysub.id
-  key_name = aws_key_pair.mykp.key_name
+  key_name               = aws_key_pair.mykp.key_name
   tags = {
     Name = "Scholar EC2"
   }
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    host        = self.public_ip
+    private_key = file("./prakash.pem")
+  }
+  provisioner "remote-exec" {
+    script = "jenkins.sh"
+  }
+  depends_on = [local_file.mypem, tls_private_key.myrsa]
+
 }
